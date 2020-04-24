36ac26e7510b9e46f3e5e5f00344de42d745dab0
Tomoki Kozuru <kozurutmk@users.noreply.github.com>
2020-04-24 14:31:15 +0900

    Update flock.rb

--- flock.rb
+++ flock.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Flock < Formula
   homepage 'https://github.com/discoteq/flock'
-  version '0.3.0'
+  version '0.4.0'
   url "https://github.com/discoteq/flock/releases/download/v#{version}/flock-#{version}.tar.xz"
-  sha256 '5f2298dbaf39bc3708e54f32705addc3d70f9aa16202d46fc08316f1237892c6'
+  sha256 '01bbd497d168e9b7306f06794c57602da0f61ebd463a3210d63c1d8a0513c5cc'
 
   def install
     system './configure', '--disable-debug',
